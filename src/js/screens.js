144deed973411d282939ec77c8e6eb2a65c4c845
Mikhail Lubarets <lumek@icloud.com>
2022-08-11 16:33:32 +0300

    Upload second homework

---
+++ src/js/screens.js
@@ -0,0 +1,24 @@
+const renderSelectLevelScreen = () => {
+  const levelPopup = document.createElement("div");
+  levelPopup.classList.add("level-popup");
+
+  window.application.renderBlock(
+    "header",
+    levelPopup,
+    "Выбери сложность",
+    "level-popup__header"
+  );
+
+  window.application.renderBlock("select-level", levelPopup);
+
+  window.application.renderBlock(
+    "active-btn",
+    levelPopup,
+    "Старт",
+    "level-popup__start-btn"
+  );
+
+  app.appendChild(levelPopup);
+};
+
+window.application.screens["select-level"] = renderSelectLevelScreen;
